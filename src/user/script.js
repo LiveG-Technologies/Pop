71e72d10146f4956f5705285734b4fe070f50570
LiveG <hi@liveg.tech>
2018-04-15 16:09:08 +0100

    Add tests for home screen

--- src/user/script.js
+++ src/user/script.js
@@ -1,9 +1,15 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
 function change(user) {
     if (user && user.uid != currentUid) {
         // Signed in.
     } else {
         // Signed out.
-        window.location.href = "../signin.html";
+        if (getURLParameter("test") != "true") {
+            window.location.href = "../signin.html";
+        }
     }
 }
 
